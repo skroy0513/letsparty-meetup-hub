8630859758795c69bd50e9b80b4151c502bd0b3b
jongmyeongbak <withpleasure@gmail.com>
2023-08-13 19:03:56 +0900

    update: 업로드 시험용 시험경로 페이지 추가

--- src/main/java/com/letsparty/web/controller/UploadController.java
+++ src/main/java/com/letsparty/web/controller/UploadController.java
@@ -9,26 +9,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/upload")
 public class UploadController {
 
+	// 첨부파일 경로제공
 	@GetMapping("/file")
 	public String file() {
 		return "page/upload/file.html";
 	}
 	
+	// 미디어 경로제공
 	@GetMapping("/media")
 	public String media() {
 		return "page/upload/media.html";
 	}
 	
+	// 파티커버 경로제공
 	@GetMapping("/cover")
 	public String cover() {
 		return "page/upload/cover.html";
 	}
 	
+	// 유저프로필 경로제공
 	@GetMapping("/profile")
 	public String profile() {
 		return "page/upload/profile.html";
 	}
 	
+	// 파일전송 시험용
+	@GetMapping("/testparty")
+	public String testparty() {
+		return "page/upload/testparty.html";
+	}
+	
+	// 파일전송불가 시험용
 	@GetMapping("/wrongpath")
 	public String wrongpath() {
 		return "page/upload/wrongpath.html";
