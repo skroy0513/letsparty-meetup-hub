359b2afa84bab8f35ce3d3c272c51a434fd3b784
juwudong <129810051+juwudong@users.noreply.github.com>
2023-08-17 19:46:41 +0900

    add: album,file.html/css 생성 (#43)
    * add: album,file.html/css 생성
    
    * fix: partyController 수정 및 album, attahment 이름변경
    
    * fix: html내 css 경로수정 및 이름변경
    
    ---------
    
    Co-authored-by: jongmyeongbak <116707497+jongmyeongbak@users.noreply.github.com>

--- src/main/java/com/letsparty/web/controller/PartyController.java
+++ src/main/java/com/letsparty/web/controller/PartyController.java
@@ -11,32 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PartyController {
 	
 	@GetMapping("/{partyNo}/member")
-	public String member(@PathVariable String partyNo) {
+	public String member(@PathVariable int partyNo) {
 		// partyNo를 사용하여 파티 멤버를 조회하고 작업을 수행합니다.
 		return "page/party/member";
 	}
 	
 	@GetMapping("/{partyNo}/modify")
-	public String modify(@PathVariable String partyNo, Model model) {
+	public String modify(@PathVariable int partyNo, Model model) {
 		// partyNo를 사용하여 파티 정보를 조회하고 작업을 수행합니다.
 		return "page/party/modify";
 	}
 	
 	@GetMapping("/{partyNo}")
-	public String home(@PathVariable String partyNo, Model model) {
+	public String home(@PathVariable int partyNo, Model model) {
 		// partyNo를 사용하여 파티 게시물을 조회합니다.
 		return "page/party/home";
 	}
 	
+	@GetMapping("/{partyNo}/attachment")
+	public String attachment(@PathVariable int partyNo){
+		// partyNo를 사용해서 파티첨부파일을 조회하고 작업을 수행합니다.
+		return"page/party/attachment";
+	}
+	
 	@GetMapping("/{partyNo}/event")
-	public String event(@PathVariable String partyNo) {
+	public String event(@PathVariable int partyNo) {
 		// partyNo를 사용하여 파티 정보를 조회하고 작업을 수행합니다.
 		return "page/party/event";
   }
-	
-	@GetMapping("/{partyNo}/attachment")
-	public String attachment(@PathVariable String partyNo){
-		// partyNo를 사용하여 파티 정보를 조회하고 작업을 수행합니다.
-		return"page/party/attachment";
-	}
 }
