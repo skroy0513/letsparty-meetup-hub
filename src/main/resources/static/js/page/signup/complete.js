23ed6ba17e8992c7a75d0b6c6e9a547bbd2e12f8
skroy0513 <skroy0513@gmail.com>
2023-08-18 14:57:17 +0900

    feat: 사진을 변경하지 않고 프로필 변경기능 구현

--- src/main/resources/static/js/page/signup/complete.js
+++ src/main/resources/static/js/page/signup/complete.js
@@ -8,10 +8,6 @@ let cropper;
 let $cancel = $("#cancel");
 let $profileForm = $("#profile-form")
 
-$cancel.on("click", function(){
-	console.log("click")
-})
-
 // b64데이터를 Blob객체로 변환하는 함수
 function b64toBlob(b64Data, contentType = '') {
 	const image_data = atob(b64Data.split(',')[1]); // data:image/gif;base64 필요없으니 떼주고, base64 인코딩을 풀어준다
@@ -27,63 +23,41 @@ function b64toBlob(b64Data, contentType = '') {
 
 	return new Blob([arraybuffer], { type: contentType });
 }
+
+// 프로필 변경을 하지 않을 경우
+$cancel.on("click", function(){
+	// 홈화면으로 돌아간다
+	window.location = "/";
+})
+
+// 사진변경 버튼을 누를 경우
 $change.on("click", function(){
+	// <input type="file">을 실행한다.
 	input.click();
 })
 
-$approve.on("click", function(){
-	console.log("approve");
-	let src = avatar.src;	
-	let blob = b64toBlob(src, 'image/png');
-	let blobURL = URL.createObjectURL(blob);
-	console.log(blobURL);
-	$("#newimg").attr("src", blobURL);
-	let file = new File([blob], "image", {type : "image/png"});
-	let formdata = new FormData();
-	formdata.append("file", file);
-	console.log(formdata.has("file"));
-	$.ajax({
-		url: "/upload/profile",
-		type: 'POST',
-		data: formdata,
-		processData: false,
-		contentType: false
-	}).done(function(response){
-		let savedName = response.savedName;
-		$("<input>").attr({
-			type: "hidden",
-			name: "filename",
-			value: savedName
-		}).appendTo($profileForm);
-		input.remove();
-		
-		console.log(savedName);
-		console.log($profileForm);
-		console.log("이미지 업로드 성공")
-		
-		$profileForm.submit();
-		
-	}).fail(function(){
-		console.log("이미지 업로드 실패")
-	})
-})
-
+// 사진 파일을 변경할 시 나타나는 이벤트
 input.addEventListener('change', function (e) {
 	let files = e.target.files;
 	let size = input.files[0].size / 1024 / 1024;
+	// 파일 사이즈가 큰 경우
 	if(size > 30) {
 		alert("이미지 파일 크기가 너무 큽니다. 30MB 이하의 파일을 업로드 해주세요.");
 		input.value = "";
 		return;
 	}
+	
+	// 파일 사이즈가 크지 않은 경우 사진 편집 창을 연다.
 	let done = function (url) {
-	input.value = '';
-	image.src = url;
-	$modal.modal('show');
+		input.value = '';
+		image.src = url;
+		$modal.modal('show');
 	};
+	
 	let reader;
 	let file;
 
+	// 사진 파일을 편집해서 변경할 경우 FileReader가 파일을 읽는다.
 	if (files && files.length > 0) {
 		file = files[0];
 		if (URL) {
@@ -98,7 +72,9 @@ input.addEventListener('change', function (e) {
 	}
 });
 
+// 모달 창을 열고 닫는다.
 $modal.on('shown.bs.modal', function () {
+	// Cropper 생성(옵션값)
 	cropper = new Cropper(image, {
 		aspectRatio: 1,
 		viewMode: 1,
@@ -112,10 +88,11 @@ $modal.on('shown.bs.modal', function () {
 	cropper = null;
 });
 
+// 사진 편집 창에서 "자르기"버튼을 누른 경우
 document.getElementById('crop').addEventListener('click', function () {
-	let initialAvatarURL;
 	let canvas;
 
+	// 편집 창을 닫는다.
 	$modal.modal('hide');
 
 	if (cropper) {
@@ -123,46 +100,54 @@ document.getElementById('crop').addEventListener('click', function () {
 			width: 160,
 			height: 160,
 		});
-		initialAvatarURL = avatar.src;
+		// 미리보기 이미지 src에 편집한 이미지의 url을 넣는다.
 		avatar.src = canvas.toDataURL();
-		// canvas.toBlob(function (blob) {
-			// let formData = new FormData();
-			// formData.append('avatar', blob, 'avatar.jpg');
-
-
-			// $.ajax('https://jsonplaceholder.typicode.com/posts', {
-			// 	method: 'POST',
-			// 	data: formData,
-			// 	processData: false,
-			// 	contentType: false,
-
-			// 	xhr: function () {
-			// 		let xhr = new XMLHttpRequest();
-
-			// 		xhr.upload.onprogress = function (e) {
-			// 		let percent = '0';
-			// 		let percentage = '0%';
-
-			// 		if (e.lengthComputable) {
-			// 			percent = Math.round((e.loaded / e.total) * 100);
-			// 			percentage = percent + '%';
-			// 			$progressBar.width(percentage).attr('aria-valuenow', percent).text(percentage);
-			// 			}
-			// 		};
-			// 		return xhr;
-			// 	},
-			// 	success: function () {
-			// 		$alert.show().addClass('alert-success').text('Upload success');
-			// 	},
-			// 	error: function () {
-			// 		avatar.src = initialAvatarURL;
-			// 		$alert.show().addClass('alert-warning').text('Upload error');
-			// 	},
-			// 	complete: function () {
-			// 		$progress.hide();
-			// 	},
-			// });
-		// });
 	}
 });
 
+
+$approve.on("click", function(){
+	let src = avatar.src;	
+	// 사진이 변경된 경우
+	if (src != "http://localhost/images/party/profile-default.png") {
+		// 미리보기 이미지의 src를 b64데이터에서 Blob객체로 변환
+		let blob = b64toBlob(src, 'image/png');
+		
+		// Blob객체를 image.png 파일로 변환
+		let file = new File([blob], "image", {type : "image/png"});
+		// 새로운 폼객체 생성후 이미지 파일 첨부
+		let formdata = new FormData();
+		formdata.append("file", file);
+		
+		// ajax로 파일을 S3로 전송후 nanoid 발급
+		$.ajax({
+			url: "/upload/profile",
+			type: 'POST',
+			data: formdata,
+			processData: false,
+			contentType: false
+		}).done(function(response){
+			// S3에서 제공하는 nanoid
+			let savedName = response.savedName;
+			// 새로운 <input>태그를 만들어서 nanoid를 필드로 삽입 
+			$("<input>").attr({
+				type: "hidden",
+				name: "filename",
+				value: savedName
+			}).appendTo($profileForm);
+			
+			// 기본 프로필 이미지가 저장된 필드 삭제
+			input.remove();
+			
+			console.log("이미지 업로드 성공");
+			
+			$profileForm.submit();
+			
+		}).fail(function(){
+			console.log("이미지 업로드 실패")
+		})
+		// 사진을 변경하지 않은 경우
+	} else {
+			$profileForm.submit();		
+	}
+})
