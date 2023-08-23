6c7a1769c05c284688d2cf96b99ae3338fc65b4e
jongmyeongbak <withpleasure@gmail.com>
2023-08-23 17:27:18 +0900

    fix: fix redundant import

--- src/main/java/com/letsparty/service/FileS3ServiceImpl.java
+++ src/main/java/com/letsparty/service/FileS3ServiceImpl.java
@@ -1,7 +1,6 @@
 package com.letsparty.service;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
