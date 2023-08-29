60718601780d5e0c6cd11685dafe578b23fd7fa1
Jiho <h2j970313@gmail.com>
2023-08-29 14:14:05 +0900

    add: 렛츠파티 게시물 상세 화면 및 컨트롤러, 기존 화면 style제거(부트스트랩 클래스에 적용됨)

--- src/main/java/com/letsparty/web/controller/LetsPartyController.java
+++ src/main/java/com/letsparty/web/controller/LetsPartyController.java
@@ -3,6 +3,7 @@ package com.letsparty.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class LetsPartyController {
 	@GetMapping("/post")
 	public String post() {
 		return "page/letsparty/post";
+	}
+	
+	// 렛츠파티 게시글 상세 화면으로 이동
+	@GetMapping("/post/{postNo}")
+	public String detail(@PathVariable int postNo) {
+		return "page/letsparty/detail";
 	}
 }
