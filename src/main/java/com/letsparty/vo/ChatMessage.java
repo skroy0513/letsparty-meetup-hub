279378c0912dcd3438c21538085ad9c7eba24069
jongmyeongbak <withpleasure@gmail.com>
2023-08-31 19:14:57 +0900

    Fix typo

--- src/main/java/com/letsparty/vo/ChatMessage.java
+++ src/main/java/com/letsparty/vo/ChatMessage.java
@@ -5,26 +5,28 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ChatMessage {
 
 	private long no;
 	private long roomNo;
 	private int type;
 	private int userNo;
-	private LocalDateTime createAt;
-	private Integer unreadCnt;
+	private LocalDateTime createdAt;
+	private Long unreadCnt;
 	private String text;
 	
 	@Builder
-	public ChatMessage(long roomNo, int type, int userNo, LocalDateTime createAt, Integer unreadCnt, String text) {
+	public ChatMessage(long roomNo, int type, int userNo, LocalDateTime createdAt, Long unreadCnt, String text) {
 		super();
 		this.roomNo = roomNo;
 		this.type = type;
 		this.userNo = userNo;
-		this.createAt = createAt;
+		this.createdAt = createdAt;
 		this.unreadCnt = unreadCnt;
 		this.text = text;
 	}
