2379ef29b8e1de0c9a66396fdd610058870ced23
juwudong <tladbsqh97@naver.com>
2023-09-02 01:04:58 +0900

    fix 모달충돌 수정

--- src/main/java/com/letsparty/web/controller/PartyController.java
+++ src/main/java/com/letsparty/web/controller/PartyController.java
@@ -202,11 +202,5 @@ public class PartyController {
 		return "page/party/event";
   }
 
-	@GetMapping("/{partyNo}/post")
-	public String post(@PathVariable int partyNo) {
-		// partyNo를 사용하여 파티 정보를 조회하고 작업을 수행합니다.
-		return "page/party/post";
-  }
-
 }
 
