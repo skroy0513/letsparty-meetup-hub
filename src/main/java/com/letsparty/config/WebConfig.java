ccdee5bdf3028922eab819f1d34b0269e7082787
skroy0513 <skroy0513@gmail.com>
2023-09-04 23:21:20 +0900

    fix: HomeInterceptor 삭제 하고 MainController에서 작성

--- src/main/java/com/letsparty/config/WebConfig.java
+++ src/main/java/com/letsparty/config/WebConfig.java
@@ -5,24 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.letsparty.web.interceptor.HomeInterceptor;
 import com.letsparty.web.interceptor.PartyInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
 	private final PartyInterceptor partyInterceptor;
-	private final HomeInterceptor homeInterceptor;
 	
 	@Autowired
-	public WebConfig(PartyInterceptor partyInterceptor, HomeInterceptor homeInterceptor) {
+	public WebConfig(PartyInterceptor partyInterceptor) {
 		this.partyInterceptor = partyInterceptor;
-		this.homeInterceptor = homeInterceptor;
+//		this.homeInterceptor = homeInterceptor;
 	}
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(partyInterceptor).addPathPatterns("/party/{partyNo}/**");
-		registry.addInterceptor(homeInterceptor).addPathPatterns("/");
 	}
 }
--- src/main/java/com/letsparty/web/controller/MainController.java
+++ src/main/java/com/letsparty/web/controller/MainController.java
@@ -1,9 +1,19 @@
 package com.letsparty.web.controller;
 
 
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.letsparty.security.user.LoginUser;
+import com.letsparty.service.LetsPartyService;
+import com.letsparty.service.PartyService;
+import com.letsparty.vo.LetsPartyPost;
+import com.letsparty.vo.Party;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,9 +21,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor 
 @SessionAttributes("signupForm")
 public class MainController {
+	
+	private final PartyService partyService;
+	private final LetsPartyService letsPartyService;
 
 	@GetMapping("/")
-	public String home() {
+	public String home(@AuthenticationPrincipal LoginUser loginUser, Model model) {
+		List<LetsPartyPost> lpPosts = letsPartyService.getPostsLimit5();
+		List<Party> parties = partyService.getPartiesLimit5();
+		
+		model.addAttribute("lpPosts", lpPosts);
+		model.addAttribute("parties", parties);
+		
+		// 로그인 상태이면 내가 가입한 파티의 정보를 생성 날짜 기준으로 최근 5개를 가져와 표시한다.
+		if (loginUser != null) {
+			List<Party> userParty = partyService.getpartyByUserId(loginUser.getId());
+			model.addAttribute("userParty", userParty);			
+		}
+		
 		return "page/main/home";
 	}
 	
