521a0688a66e2211811791cc59a363f876a4881f
skroy0513 <skroy0513@gmail.com>
2023-09-05 16:07:28 +0900

    style: mypage 화면 개발

--- src/main/java/com/letsparty/web/controller/MyController.java
+++ src/main/java/com/letsparty/web/controller/MyController.java
@@ -1,13 +1,16 @@
 package com.letsparty.web.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.letsparty.security.user.LoginUser;
 import com.letsparty.service.UserProfileService;
 import com.letsparty.vo.UserProfile;
 import com.letsparty.web.form.UserProfileForm;
@@ -23,6 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 public class MyController {
 
 	private final UserProfileService myService;
+	
+	@GetMapping
+	public String myPage(@AuthenticationPrincipal LoginUser loginUser, Model model) {
+		
+		return "/page/main/mypage";
+	}
 	
 	@PostMapping("/profile")
 	public String changeProfile(UserProfileForm userProfileForm) {
