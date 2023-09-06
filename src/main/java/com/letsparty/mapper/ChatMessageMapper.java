88d7294ddeeae5c40e3428a9b659d9dfc03bc7ab
jongmyeongbak <withpleasure@gmail.com>
2023-09-06 15:03:03 +0900

    add: 안읽은수 감소 매퍼 구현

--- src/main/java/com/letsparty/mapper/ChatMessageMapper.java
+++ src/main/java/com/letsparty/mapper/ChatMessageMapper.java
@@ -11,4 +11,5 @@ public interface ChatMessageMapper {
 	void insertChatMessage(ChatMessage chatMessage);
 	
 	void decreaseUnreadCntByRoomNoAndLastReadMessageNo(@Param("roomNo") long roomNo, @Param("lastReadMessageNo") long lastReadMessageNo);
+	void decreaseUnreadCntByRoomIdAndLastReadMessageNo(@Param("roomId") String roomId, @Param("lastReadMessageNo") long lastReadMessageNo);
 }
