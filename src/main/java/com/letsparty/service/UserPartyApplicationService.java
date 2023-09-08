9a4e6687246616fe80e9ad08088617ce431e74a1
skroy0513 <skroy0513@gmail.com>
2023-09-08 16:49:11 +0900

    style: 홈 post preview 화면 구현

--- src/main/java/com/letsparty/service/UserPartyApplicationService.java
+++ src/main/java/com/letsparty/service/UserPartyApplicationService.java
@@ -70,6 +70,7 @@ public class UserPartyApplicationService {
 		}
 		
 		addUserPartyApplicationWithApproved(partyNo, userId, 8, userProfile);
+		//TODO 파티 가입후 해당 파티의 인원 증가 메소드 필요...
 		
 		return true;
 	}
