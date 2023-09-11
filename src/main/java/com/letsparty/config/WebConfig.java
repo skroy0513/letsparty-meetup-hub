b0b1b9bd1d2cc948b4498651bb358cbe0361dd53
jongmyeongbak <withpleasure@gmail.com>
2023-09-11 14:58:37 +0900

    style: Arrange code style

--- src/main/java/com/letsparty/config/WebConfig.java
+++ src/main/java/com/letsparty/config/WebConfig.java
@@ -13,25 +13,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-   private final PartyInterceptor partyInterceptor;
-   private final NavbarInterceptor navbarInterceptor;
+	private final PartyInterceptor partyInterceptor;
+	private final NavbarInterceptor navbarInterceptor;
 
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-      registry.addInterceptor(navbarInterceptor)
-            .addPathPatterns("/", "/party/**", "/letsparty/**", "/my/**", "/party-create")
-            .excludePathPatterns("/css/**", "/js/**", "/images/**",
-            		"/party/{partyNo}",
-            		"/party/{partyNo}/read/{postNo}",
-            		"/party/{partyNo}/post/{postId}/comment",
-            		"/party/{partyNo}/withdraw/{upaNo}",
-            		"/my/profile/{profileNo}");
-      registry.addInterceptor(partyInterceptor)
-            .addPathPatterns("/party/{partyNo}/**")
-            .excludePathPatterns(
-            		"/party/{partyNo}",
-            		"/party/{partyNo}/read/{postNo}",
-            		"/party/{partyNo}/post/{postId}/comment",
-            		"/party/{partyNo}/withdraw/{upaNo}");
-   }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(navbarInterceptor)
+				.addPathPatterns("/", "/party/**", "/letsparty/**", "/my/**", "/party-create")
+				.excludePathPatterns("/css/**", "/js/**", "/images/**",
+						"/party/{partyNo}",
+						"/party/{partyNo}/read/{postNo}",
+						"/party/{partyNo}/post/{postId}/comment",
+						"/party/{partyNo}/withdraw/{upaNo}",
+						"/my/profile/{profileNo}");
+		registry.addInterceptor(partyInterceptor)
+				.addPathPatterns("/party/{partyNo}/**")
+				.excludePathPatterns(
+						"/party/{partyNo}",
+						"/party/{partyNo}/read/{postNo}",
+						"/party/{partyNo}/post/{postId}/comment",
+						"/party/{partyNo}/withdraw/{upaNo}");
+	}
 }
