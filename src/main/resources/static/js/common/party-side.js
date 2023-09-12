a73d5ec404d1c425f890b3fc5ddff73019404cd8
jongmyeongbak <withpleasure@gmail.com>
2023-09-12 10:39:50 +0900

    style: Style chat popup disposition change

--- src/main/resources/static/js/common/party-side.js
+++ src/main/resources/static/js/common/party-side.js
@@ -1,3 +1,20 @@
-/**
- * 
- */
+function openPopup(url) {
+  let width = 400;
+  let height = 644;
+  let left = (window.innerWidth - width) / 2 + window.screenX;
+  let top = (window.innerHeight - height) / 2 + window.screenY;
+
+  console.log(left);
+  console.log(top);
+  let popup = window.open(
+    url,
+    "_blank",
+    "width=" + width + ", height=" + height + ", left=" + left + ", top=" + top
+  );
+
+  if (popup) {
+    popup.focus();
+  } else {
+    alert("팝업 차단을 해제해 주세요.");
+  }
+}
